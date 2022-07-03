7f10c1043472aa736afb167efa08c6e80326ee9c
Furqan Hameed <62751049+mutantstroke@users.noreply.github.com>
2022-07-03 14:18:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Happy to be at Archis Academy.
+          Excited to be at Archis Academy.
         </p>
         <a
           className="App-link"
